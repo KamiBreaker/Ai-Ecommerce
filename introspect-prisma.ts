5a094f2ad4d4acfd1d0cca22370ec2f441058dbb
KamiBreaker <furykinov@gmail.com>
2026-02-28 18:27:30 +0600

    fix(prisma): Restore adapter configuration for introspect utility

--- introspect-prisma.ts
+++ introspect-prisma.ts
@@ -1,8 +1,12 @@
 import 'dotenv/config'
-import { PrismaClient } from '@prisma/client'
+const { PrismaClient } = require('@prisma/client');
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
 
 async function test() {
-    const prisma = new PrismaClient()
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL })
+    const adapter = new PrismaPg(pool)
+    const prisma = new PrismaClient({ adapter })
 
     console.log('--- Prisma Instance Keys ---');
     console.log(Object.keys(prisma).join(', '));

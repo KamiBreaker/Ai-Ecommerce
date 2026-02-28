c0fffa96b51b68f96fc2964848fdb0a127cf6be4
KamiBreaker <furykinov@gmail.com>
2026-02-28 18:19:18 +0600

    chore: connect supabase database

--- introspect-prisma.ts
+++ introspect-prisma.ts
@@ -1,12 +1,8 @@
 import 'dotenv/config'
 import { PrismaClient } from '@prisma/client'
-import { Pool } from 'pg'
-import { PrismaPg } from '@prisma/adapter-pg'
 
 async function test() {
-    const pool = new Pool({ connectionString: process.env.DATABASE_URL })
-    const adapter = new PrismaPg(pool)
-    const prisma = new PrismaClient({ adapter })
+    const prisma = new PrismaClient()
 
     console.log('--- Prisma Instance Keys ---');
     console.log(Object.keys(prisma).join(', '));
